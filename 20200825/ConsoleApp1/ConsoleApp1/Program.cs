38b1e1d898b76dee6e46bdf9d749adac4d7d38ad
cinthia <cinthia.denett@davinci.edu.ar>
2020-08-31 11:55:05 -0300

    ejercicio del 20200825

---
+++ 20200825/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            int dado = 1;
+            if (dado == 1)
+            {
+                string mensaje = "te ganaste un auto";
+                Console.WriteLine(mensaje);
+
+            }
+            else if (dado == 2)
+            {
+                string mensaje = "te ganaste una moto";
+                Console.WriteLine(mensaje);
+
+            }
+            else 
+            {
+               string mensaje = "segui particípando";
+            }
+            Console.WriteLine(mensaje); 
+        }
+    }
+}
